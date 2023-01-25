566f8b8fce980b758774aed89c67c77374fca13b
sonny-ipssi <s.lallier@ecole-ipssi.net>
2023-01-25 12:51:34 +0100

    Add: front layout

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,9 +1,24 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import App from "./App";
+import Breadcrumb from "./components/Breadcrumb/Breadcrumb";
+import Layout from "./components/Layout";
+import Post from "./components/Post";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+	<Layout>
+		<BrowserRouter>
+			<Breadcrumb />
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/post/:postId" element={<Post />} />
+			</Routes>
+		</BrowserRouter>
+	</Layout>
+);
