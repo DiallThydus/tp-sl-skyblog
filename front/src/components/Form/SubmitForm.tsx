09602bf12f8991f33effd9d459567098b322aa21
sonny-ipssi <s.lallier@ecole-ipssi.net>
2023-01-27 22:00:54 +0100

    Add: edit & delete comment

--- front/src/components/Form/SubmitForm.tsx
+++ front/src/components/Form/SubmitForm.tsx
@@ -1,16 +1,20 @@
+import { CSSProperties } from "react";
+
 interface SubmitFormProps {
   label?: string;
   type?: "submit" | "reset" | "button";
   loading?: boolean;
+  style?: CSSProperties;
 }
 export default function SubmitForm({
   label = "Send",
   type = "submit",
   loading = false,
+  style = {},
 }: SubmitFormProps) {
   return (
     <div className="form-field">
-      <button type={type} disabled={loading}>
+      <button type={type} disabled={loading} style={style}>
         {label}
       </button>
     </div>
